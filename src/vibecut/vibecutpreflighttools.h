29ee592e6e27873bf68fdc9e4a98bb4c818ff568
halthinks <halthinks@gmail.com>
2026-08-31 16:40:36 -0700

    vibecut: expose reusable project preflight

--- src/vibecut/vibecutpreflighttools.h
+++ src/vibecut/vibecutpreflighttools.h
@@ -1,7 +1,10 @@
 /* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
 #pragma once
 
+#include <QJsonObject>
+
 class VibeCutToolSurface;
 class QString;
 
+QJsonObject vibeCutProjectPreflight();
 bool registerVibeCutPreflightTools(VibeCutToolSurface &surface, QString *error = nullptr);
