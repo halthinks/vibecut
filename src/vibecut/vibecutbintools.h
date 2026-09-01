934f8014f284bce0a522990acf3e321cd0c6de5a
halthinks <halthinks@gmail.com>
2026-08-31 23:22:57 -0700

    fix: provide concrete undo stack type to bin tools

--- src/vibecut/vibecutbintools.h
+++ src/vibecut/vibecutbintools.h
@@ -1,5 +1,9 @@
 /* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
 #pragma once
+
+#include "doc/docundostack.hpp"
+
 #include <QString>
+
 class VibeCutToolSurface;
 bool registerVibeCutBinTools(VibeCutToolSurface &surface, QString *error = nullptr);
