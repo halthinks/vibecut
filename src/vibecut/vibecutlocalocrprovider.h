4eb96234137634bb558a972d1afb06f7a9f777a9
halthinks <halthinks@gmail.com>
2026-09-03 09:41:33 -0700

    Declare built-in local OCR provider

---
+++ src/vibecut/vibecutlocalocrprovider.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
+#pragma once
+
+#include <QString>
+
+QString vibeCutTesseractExecutable();
+QString vibeCutOcrPython();
+QString vibeCutOcrScript();
+void ensureVibeCutLocalOcrProviderRegistered();
