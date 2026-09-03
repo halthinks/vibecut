8ed3738ec866cebb2fdb62d26876d1e2a8105926
halthinks <halthinks@gmail.com>
2026-09-03 14:43:02 -0700

    Add governed hybrid search contract

---
+++ src/vibecut/vibecuthybridsearch.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
+#pragma once
+
+class QString;
+class VibeCutToolSurface;
+
+bool registerVibeCutHybridSearchTools(VibeCutToolSurface &surface, QString *error = nullptr);
