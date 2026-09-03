aacb0f94ad89e3693142d753014f5b3522650121
halthinks <halthinks@gmail.com>
2026-09-03 11:07:23 -0700

    Add local DETR object-provider interface

---
+++ src/vibecut/vibecutlocalobjectprovider.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
+#pragma once
+
+#include <QString>
+
+void ensureVibeCutLocalObjectProviderRegistered();
+QString vibeCutObjectDetectionScript();
