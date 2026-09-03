e5762bb5dde5a2b96b346d354c113a3ab84025f6
halthinks <halthinks@gmail.com>
2026-09-03 13:17:43 -0700

    Add read-only media-index document snapshot

---
+++ src/vibecut/vibecutmediaindexdocuments.cpp
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL */
+#include "vibecutmediaindex.h"
+
+QList<VibeCutMediaDocument> VibeCutMediaIndex::documents() const
+{
+    return m_documents;
+}
