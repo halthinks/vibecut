79c84397df9f19713298f319ce76ea2504afbfe7
halthinks <halthinks@gmail.com>
2026-09-04 14:09:46 -0700

    Export clean-room runtime session state and provider APIs

--- runtime/src/halthinks_runtime/__init__.py
+++ runtime/src/halthinks_runtime/__init__.py
@@ -2,17 +2,43 @@
 # SPDX-License-Identifier: LicenseRef-halthinks-Proprietary
 
 from .contracts import EditPlan, PlanOperation, PlanValidationError, validate_plan
+from .evidence import EvidenceError, EvidenceRecord, EvidenceStore
+from .jobs import JobError, JobManager, JobRecord, JobState
 from .policy import ToolPolicy, ToolRisk, TrustMode
-from .revision import RevisionGate, StaleRevisionError
+from .protocol import AdapterClient, Envelope, ProtocolError, decode_line, request
+from .providers import ModelProvider, ModelRequest, ProviderClient, ProviderError
+from .revision import RevisionError, RevisionGate, StaleRevisionError
+from .session import ExecutionResult, RuntimeSession, SessionError, SessionHello
 
 __all__ = [
+    "AdapterClient",
     "EditPlan",
+    "Envelope",
+    "EvidenceError",
+    "EvidenceRecord",
+    "EvidenceStore",
+    "ExecutionResult",
+    "JobError",
+    "JobManager",
+    "JobRecord",
+    "JobState",
+    "ModelProvider",
+    "ModelRequest",
     "PlanOperation",
     "PlanValidationError",
-    "validate_plan",
+    "ProtocolError",
+    "ProviderClient",
+    "ProviderError",
+    "RevisionError",
+    "RevisionGate",
+    "RuntimeSession",
+    "SessionError",
+    "SessionHello",
+    "StaleRevisionError",
     "ToolPolicy",
     "ToolRisk",
     "TrustMode",
-    "RevisionGate",
-    "StaleRevisionError",
+    "decode_line",
+    "request",
+    "validate_plan",
 ]
